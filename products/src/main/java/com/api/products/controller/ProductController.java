2a1a99648ff81094017dbd8c22607be0386a62d9
phfuark <paulohsilva2007@gmail.com>
2025-01-18 20:42:48 -0300

    18.01.2025 - Configurando Controller e Repository

--- products/src/main/java/com/api/products/controller/ProductController.java
+++ products/src/main/java/com/api/products/controller/ProductController.java
@@ -1,5 +1,14 @@
 package com.api.products.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
 public class ProductController {
-    
+
+    @GetMapping("/")
+    public String rout(){
+        return "API está funcionando!";
+    }
 }
--- products/src/main/java/com/api/products/repository/ProductRepository.java
+++ products/src/main/java/com/api/products/repository/ProductRepository.java
@@ -1,5 +1,11 @@
 package com.api.products.repository;
 
-public interface ProductRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.api.products.models.ProductModel;
+
+@Repository
+public interface ProductRepository extends CrudRepository<ProductModel, Long>{
     
 }
