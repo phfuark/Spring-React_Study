e60f1ab048814efe5b41db4c3cd1d2a4b8cabf24
Paulo Henrique Preciliano da Silva <47903305832@>
2025-01-21 14:43:36 -0300

    21.01.2025 - Criando Front e alguns componentes

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Table from './components/Table';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <Table/>
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/components/Form.js
@@ -0,0 +1,5 @@
+function Form(){
+    return <h1 className="text-red-700">Form</h1>
+}
+
+export default Form
